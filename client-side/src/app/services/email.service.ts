393c2a29292057a43cd24a1f2782920c462287d2
dominic <dominicjventre@gmail.com>
2018-08-07 10:29:16 -0400

    Email service is ready

---
+++ client-side/src/app/services/email.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class EmailService {
+
+  private _TRAINER_GRADE_REMINDER = 'trainerGradeReminder';
+  private _VP_BATCH_STATUS_REPORT = 'vpBatchStatusReport';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public sendEmail(email_type: string, interval: number, delay: number) {
+    return this.httpClient.post('/emails/startSchedule', {
+      email_type,
+      interval,
+      delay
+    });
+  }
+
+  get VP_BATCH_STATUS_REPORT() {
+    return this._VP_BATCH_STATUS_REPORT;
+  }
+
+  get TRAINER_GRADE_REMINDER() {
+    return this._TRAINER_GRADE_REMINDER;
+  }
+
+}
